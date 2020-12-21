067748630245994b7c352d79ba5dd4e6b6c590ff
mainamuriuki274 <43462574+mainamuriuki274@users.noreply.github.com>
2020-12-21 13:41:36 +0300

    Add files via upload

---
+++ image_difference.py
@@ -0,0 +1,39 @@
+# import the necessary packages
+from skimage.measure import compare_ssim
+import argparse
+import imutils
+from cv2 import cv2
+# load the two input images
+imageA = cv2.imread(r"C:/Users/hp/Images/first_image.png")
+imageB = cv2.imread(r"C:/Users/hp/Images/second_image.png")
+imageA = cv2.resize(imageA, (224, 224)) 
+imageB = cv2.resize(imageB, (224, 224)) 
+# convert the images to grayscale
+grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+# compute the Structural Similarity Index (SSIM) between the two
+# images, ensuring that the difference image is returned
+(score, diff) = compare_ssim(grayA, grayB, full=True)
+diff = (diff * 255).astype("uint8")
+print("SSIM: {}".format(score))
+# threshold the difference image, followed by finding contours to
+# obtain the regions of the two input images that differ
+thresh = cv2.threshold(diff, 0, 255,
+	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+# loop over the contours
+for c in cnts:
+	# compute the bounding box of the contour and then draw the
+	# bounding box on both input images to represent where the two
+	# images differ
+	(x, y, w, h) = cv2.boundingRect(c)
+	cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
+	cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+# show the output images
+cv2.imshow("Original", imageA)
+cv2.imshow("Modified", imageB)
+#cv2.imshow("Diff", diff)
+#cv2.imshow("Thresh", thresh)
+cv2.waitKey(0)
